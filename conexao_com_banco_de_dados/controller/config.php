7ecbbf1ed08bb9aaf01536932d83ff8e76414b30
Mateus Almeida <124947122+oestrangeiro@users.noreply.github.com>
2024-08-28 12:46:32 -0300

    Update config.php
    vazei sem querer a senha do bancokkkkkkkkkkkk

--- conexao_com_banco_de_dados/controller/config.php
+++ conexao_com_banco_de_dados/controller/config.php
@@ -1,8 +1,8 @@
 <?php
 	// Arquivo de configuração do banco de dados
 	$hostname = 'localhost';
-	$username_db = 'admin';
-	$password_db = 'admin0000';
+	$username_db = 'root';
+	$password_db = '';
 	$database_name = 'contatos_db';
 
 	$conn = mysqli_connect($hostname, $username_db, $password_db, $database_name);
